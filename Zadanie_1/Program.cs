78c30da2246f93ba8d5a872de0ab31308cd56477
Grumel4 <monah_www@mail.ru>
2024-03-13 23:39:29 +0300

    seve

---
+++ Zadanie_1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+//Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+//Использовать рекурсию, не использовать циклы.
+
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        // Задаем значения M и N
+        int m = 1;
+        int n = 10;
+
+        // Выводим числа от M до N
+        PrintNumbers(m, n);
+    }
+
+    static void PrintNumbers(int m, int n)
+    {
+        // Базовый случай:
+        // Если M больше N, то рекурсия завершается
+        if (m > n)
+        {
+            return;
+        }
+
+        // Рекурсивный случай:
+        // Выводим M и рекурсивно вызываем функцию для N-1
+        Console.WriteLine(m);
+        PrintNumbers(m + 1, n);
+    }
+}
+
